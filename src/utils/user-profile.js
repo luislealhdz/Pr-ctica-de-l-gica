56b3219721929e6b7b47764b28485a64abbbd78a
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-01-13 01:40:48 -0600

    🎯 FEAT / User profile exercise

--- src/utils/user-profile.js
+++ src/utils/user-profile.js
@@ -2,12 +2,18 @@
 
 const movies = [];
 
-const username = promt("Escribe tu nombre de usuario");
-const age = promt("Escribe tu edad");
+const username = prompt("Escribe tu nombre de usuario");
+const age = prompt("Escribe tu edad");
 
 const totalMovies = prompt("¿Cuántas películas favoritas tienes?");
 
-for (let i = 0; i <= parseInt(totalMovies); i++) {
+for (let i = 0; i < parseInt(totalMovies); i++) {
     const movie = prompt(`Película ${i + 1}`);
     movies.push(movie);
+}
+
+console.log(username);
+console.log(age);
+for (const film of movies) {
+    console.log(`The film ${film} is one of my favorites`);
 }
